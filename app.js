7dbd5166519678ecbb0aa133745fb9cbc2aa313b
Dhruvil <prajapatidhruvil.a@gmail.com>
2024-08-01 10:18:31 +0530

    EJS completed.

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+// app.set('view engine', 'pug');
 
 // setup for handlebars bcz  is not inbuilt module of express
-app.engine('hbs', engine({
-    extname: "hbs",
-    // defaultLayout: "false",
-    // layoutsDir: "views/",
-    defaultLayout: "main-layout",
-    layoutsDir: "views/layouts/"
-}));
-app.set("view engine", 'hbs');
-
-// app.set('view engine', 'pug');
-app.set('views', 'views');
-
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.engine('hbs', engine({
+//     extname: "hbs",
+//     // defaultLayout: "false",
+//     // layoutsDir: "views/",
+//     defaultLayout: "main-layout",
+//     layoutsDir: "views/layouts/"
+// }));
+// app.set("view engine", 'hbs');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
@@ -28,7 +29,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: "Page Not Found" });
+    res.status(404).render('404', { pageTitle: "Page Not Found", path: 'ERROR' });
 })
 
 app.listen(4000, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,28 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-    res.render('add-product', { pageTitle: "Add Product", activeAddProduct: true, formCSS: true })
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 });
-router.post("/add-product", (req, res, next) => {
-    // console.log(req.body);
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title });
-    res.redirect("/");
+    res.redirect('/');
 });
 
 exports.routes = router;
